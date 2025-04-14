5a5af242458f8c922a39ff610a47af73cff1f51a
chris-mc1 <29151206+chris-mc1@users.noreply.github.com>
2025-04-14 19:14:39 +0000

    add CoffeeMaker Bean container select

--- custom_components/homeconnect_ws/entity_descriptions/consumer_products.py
+++ custom_components/homeconnect_ws/entity_descriptions/consumer_products.py
@@ -107,6 +107,12 @@ CONSUMER_PRODUCTS_ENTITY_DESCRIPTIONS: _EntityDescriptionsType = {
             device_class=SensorDeviceClass.ENUM,
             has_state_translation=True,
         ),
+        HCSelectEntityDescription(
+            key="select_bean_container",
+            entity="ConsumerProducts.CoffeeMaker.Option.BeanContainerSelection",
+            device_class=SensorDeviceClass.ENUM,
+            has_state_translation=True,
+        ),
     ],
     "switch": [
         HCSwitchEntityDescription(
