4fba6c7854c95d2a4aaa82455214095acd833ef5
chris-mc1 <29151206+chris-mc1@users.noreply.github.com>
2025-04-15 15:40:26 +0000

    add Common Remote start allowed

--- custom_components/homeconnect_ws/entity_descriptions/common.py
+++ custom_components/homeconnect_ws/entity_descriptions/common.py
@@ -125,6 +125,12 @@ COMMON_ENTITY_DESCRIPTIONS: _EntityDescriptionsType = {
             value_off={"Off", "Confirmed"},
             entity_category=EntityCategory.DIAGNOSTIC,
         ),
+        HCBinarySensorEntityDescription(
+            key="binary_remote_start_allowed",
+            entity="BSH.Common.Status.RemoteControlStartAllowed",
+            entity_registry_enabled_default=False,
+            entity_category=EntityCategory.CONFIG,
+        ),
     ],
     "program": [
         HCSelectEntityDescription(
