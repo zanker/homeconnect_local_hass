04985c606e10bcaabb711980245b875be31cef8a
chris-mc1 <29151206+chris-mc1@users.noreply.github.com>
2025-04-15 15:23:12 +0000

    catch errors while creating entity

--- custom_components/homeconnect_ws/helpers.py
+++ custom_components/homeconnect_ws/helpers.py
@@ -2,11 +2,14 @@
 
 from __future__ import annotations
 
+import logging
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from . import HCData
     from .entity import HCEntity
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def create_entities(
@@ -17,13 +20,16 @@ def create_entities(
     for entity_key, entity_class in entities_classes.items():
         if entity_key in runtime_data.available_entity_descriptions:
             for entity_description in runtime_data.available_entity_descriptions[entity_key]:
-                entities.add(
-                    entity_class(
+                _LOGGER.debug("Creating Entity %s", entity_description.key)
+                try:
+                    entity = entity_class(
                         entity_description=entity_description,
                         appliance=runtime_data.appliance,
                         device_info=runtime_data.device_info,
                     )
-                )
+                except Exception:
+                    _LOGGER.exception("Failed to create Entity %s", entity_description.key)
+                entities.add(entity)
     return entities
 
 
