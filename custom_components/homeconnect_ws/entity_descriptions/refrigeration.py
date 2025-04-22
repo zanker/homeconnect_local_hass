c3a3c34e985973418adc913445ca663ae0033756
chris-mc1 <29151206+chris-mc1@users.noreply.github.com>
2025-04-22 14:17:02 +0000

    add Refrigeration SetpointTemperature

--- custom_components/homeconnect_ws/entity_descriptions/refrigeration.py
+++ custom_components/homeconnect_ws/entity_descriptions/refrigeration.py
@@ -3,11 +3,13 @@
 from __future__ import annotations
 
 from homeassistant.components.binary_sensor import BinarySensorDeviceClass
+from homeassistant.components.number import NumberDeviceClass, NumberMode
 from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.const import UnitOfTemperature
 
 from .descriptions_definitions import (
     HCBinarySensorEntityDescription,
+    HCNumberEntityDescription,
     HCSensorEntityDescription,
     _EntityDescriptionsType,
 )
@@ -50,5 +52,23 @@ REFRIGERATION_ENTITY_DESCRIPTIONS: _EntityDescriptionsType = {
             device_class=SensorDeviceClass.TEMPERATURE,
             native_unit_of_measurement=UnitOfTemperature.CELSIUS,
         )
+    ],
+    "number": [
+        HCNumberEntityDescription(
+            key="number_setpoint_freezer",
+            entity="Refrigeration.FridgeFreezer.Setting.SetpointTemperatureFreezer",
+            native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+            device_class=NumberDeviceClass.TEMPERATURE,
+            mode=NumberMode.AUTO,
+            step=1,
+        ),
+        HCNumberEntityDescription(
+            key="number_setpoint_refrigerator",
+            entity="Refrigeration.FridgeFreezer.Setting.SetpointTemperatureRefrigerator",
+            native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+            device_class=NumberDeviceClass.TEMPERATURE,
+            mode=NumberMode.AUTO,
+            step=1,
+        ),
     ],
 }
