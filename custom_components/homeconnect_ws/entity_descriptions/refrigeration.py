a1c10588552f57534ad1862b09a0c3ba041e8ffd
chris-mc1 <29151206+chris-mc1@users.noreply.github.com>
2025-04-22 13:29:14 +0000

    add Refrigeration Door

--- custom_components/homeconnect_ws/entity_descriptions/refrigeration.py
+++ custom_components/homeconnect_ws/entity_descriptions/refrigeration.py
@@ -2,9 +2,7 @@
 
 from __future__ import annotations
 
-from homeassistant.components.binary_sensor import (
-    BinarySensorDeviceClass,
-)
+from homeassistant.components.binary_sensor import BinarySensorDeviceClass
 
 from .descriptions_definitions import (
     HCBinarySensorEntityDescription,
@@ -14,18 +12,33 @@ from .descriptions_definitions import (
 REFRIGERATION_ENTITY_DESCRIPTIONS: _EntityDescriptionsType = {
     "binary_sensor": [
         HCBinarySensorEntityDescription(
-            key="binary_sensor_door_state",
+            key="binary_sensor_freezer_door_state",
             entity="Refrigeration.Common.Status.Door.Freezer",
             device_class=BinarySensorDeviceClass.DOOR,
             value_on={"Open"},
             value_off={"Closed"},
         ),
         HCBinarySensorEntityDescription(
-            key="binary_sensor_door_state",
+            key="binary_sensor_fridge_door_state",
             entity="Refrigeration.Common.Status.Door.Refrigerator",
             device_class=BinarySensorDeviceClass.DOOR,
             value_on={"Open"},
             value_off={"Closed"},
         ),
-    ]
+        HCBinarySensorEntityDescription(
+            key="binary_sensor_freezer_door_state",
+            entity="Refrigeration.FridgeFreezer.Status.DoorFreezer",
+            device_class=BinarySensorDeviceClass.DOOR,
+            value_on={"Open"},
+            value_off={"Closed"},
+        ),
+        HCBinarySensorEntityDescription(
+            key="binary_sensor_fridge_door_state",
+            entity="Refrigeration.FridgeFreezer.Status.DoorRefrigerator",
+            device_class=BinarySensorDeviceClass.DOOR,
+            value_on={"Open"},
+            value_off={"Closed"},
+        ),
+    ],
+    "sensor": [],
 }
