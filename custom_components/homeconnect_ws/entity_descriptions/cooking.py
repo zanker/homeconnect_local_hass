bd32026eac0485eb97da5aed57524780d04e6c98
chris-mc1 <29151206+chris-mc1@users.noreply.github.com>
2025-04-28 15:25:08 +0000

    add Oven WaterTank

--- custom_components/homeconnect_ws/entity_descriptions/cooking.py
+++ custom_components/homeconnect_ws/entity_descriptions/cooking.py
@@ -2,15 +2,52 @@
 
 from __future__ import annotations
 
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-)
+import re
+from typing import TYPE_CHECKING
+
+from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.const import UnitOfTime
 
+from custom_components.homeconnect_ws.helpers import get_groups_from_regex
+
 from .descriptions_definitions import (
+    EntityDescriptions,
     HCSensorEntityDescription,
     _EntityDescriptionsDefinitionsType,
 )
+
+if TYPE_CHECKING:
+    from homeconnect_websocket import HomeAppliance
+
+
+def generate_oven_status(appliance: HomeAppliance) -> EntityDescriptions:
+    pattern = re.compile(r"^Cooking\.Oven\.Status\.Cavity\.(.*)\..*$")
+    groups = get_groups_from_regex(appliance, pattern)
+    descriptions = EntityDescriptions(event_sensor=[])
+    for group in groups:
+        group_name = int(group[0])
+        if len(groups) == 1:
+            group_name = ""
+
+        # Water Tank
+        entities = (
+            f"Cooking.Oven.Status.Cavity.{group[0]}.WaterTankUnplugged",
+            f"Cooking.Oven.Status.Cavity.{group[0]}.WaterTankEmpty",
+        )
+        if all(entity in appliance.entities for entity in entities):
+            descriptions["event_sensor"].append(
+                HCSensorEntityDescription(
+                    key=f"sensor_oven_water_tank_{group[0]}",
+                    translation_key="sensor_oven_water_tank",
+                    translation_placeholders={"group_name": group_name},
+                    entities=entities,
+                    device_class=SensorDeviceClass.ENUM,
+                    options=["unplugged", "empty", "ok"],
+                )
+            )
+
+    return descriptions
+
 
 COOKING_ENTITY_DESCRIPTIONS: _EntityDescriptionsDefinitionsType = {
     "sensor": [
@@ -32,5 +69,6 @@ COOKING_ENTITY_DESCRIPTIONS: _EntityDescriptionsDefinitionsType = {
             device_class=SensorDeviceClass.DURATION,
             native_unit_of_measurement=UnitOfTime.SECONDS,
         ),
-    ]
+    ],
+    "dynamic": [generate_oven_status],
 }
