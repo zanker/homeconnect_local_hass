64e31578ddcc69608497cba493481291bad9a1c3
Jelmer <jelmer_bruijn@hotmail.com>
2025-07-03 01:12:37 +0200

    Update common.py
    Added 'Start in' as number instead of sensor

--- custom_components/homeconnect_ws/entity_descriptions/common.py
+++ custom_components/homeconnect_ws/entity_descriptions/common.py
@@ -267,13 +267,6 @@ COMMON_ENTITY_DESCRIPTIONS: _EntityDescriptionsDefinitionsType = {
             has_state_translation=True,
         ),
         HCSensorEntityDescription(
-            key="sensor_start_in",
-            entity="BSH.Common.Option.StartInRelative",
-            device_class=SensorDeviceClass.DURATION,
-            native_unit_of_measurement=UnitOfTime.SECONDS,
-            suggested_unit_of_measurement=UnitOfTime.HOURS,
-        ),
-        HCSensorEntityDescription(
             key="sensor_count_started",
             entity="BSH.Common.Status.Program.All.Count.Started",
             entity_category=EntityCategory.DIAGNOSTIC,
@@ -353,6 +346,13 @@ COMMON_ENTITY_DESCRIPTIONS: _EntityDescriptionsDefinitionsType = {
         HCNumberEntityDescription(
             key="number_duration",
             entity="BSH.Common.Option.Duration",
+            device_class=NumberDeviceClass.DURATION,
+            native_unit_of_measurement=UnitOfTime.SECONDS,
+            mode=NumberMode.AUTO,
+        ),
+        HCNumberEntityDescription(
+            key="number_start_in",
+            entity="BSH.Common.Option.StartInRelative",
             device_class=NumberDeviceClass.DURATION,
             native_unit_of_measurement=UnitOfTime.SECONDS,
             mode=NumberMode.AUTO,
